44d1bc3c85ee46cd3456be4ea2058bc82503a386
Santiago <142843041+SantiagoTucci@users.noreply.github.com>
2023-11-01 12:06:39 -0300

    vistaPagarJS

--- VistaPagar/code.js
+++ VistaPagar/code.js
@@ -1,22 +1,63 @@
+/*ETIQUETAS*/
 const form = document.getElementById("form");
 
-const numero = document.getElementById("campoTarjeta");
-const vencimiento = document.getElementById("campoVencimiento");
-const cvc = document.getElementById("campoContrasenia");
-const nombre = document.getElementById("campoNombreApellido");
+const numeroG = document.getElementById("campoTarjeta");
+const vencimientoG = document.getElementById("campoVencimiento");
+const cvcG = document.getElementById("campoContrasenia");
+const nombreG = document.getElementById("campoNombreApellido");
 
-const btnAtras = document.getElementById("btnAtras");
+const checkBoxG = document.getElementById("checkBox")
 const btnPagar = document.getElementById("btnPagar");
 
-form.addEventListener("submit", e=>{
+/*MENSAJE SOBRESALIENTE*/
+form.addEventListener("submit", function(e){
     e.preventDefault()
-    if(numero.value.length < 20) {
+    if(numeroG.value.length > 20) {
         alert("Numero de tarjeta invalido")
     }
-    if(nombre.value.length < 6) {
+    if(nombreG.value.length < 6) {
         alert("El nombre no es valido")
     }
-    if(cvc.value.length < 3) {
+    if(cvcG.value.length < 3) {
         alert("Contraseña no valida")
     }
+    if(!checkBoxG.checked){
+        alert("Por favor, marca el checkbox antes de enviar el formulario.");
+    }
 });
+
+/*GUARDAR EN LOCAL STORAGE*/
+function guardarEnLocalStorage() {
+    var numero = numeroG.value;
+    var vencimiento = vencimientoG.value;
+    var cvc = cvcG.value;
+    var nombre = nombreG.value;
+    
+    // Verificar si se han proporcionado valores
+    if (numero && vencimiento && cvc && nombre) {
+        /*AGREGAR EL OBJETO(tarjeta) A UN ARREGLO(usuarios)*/
+      var usuario = JSON.parse(localStorage.getItem("usuarios")) || [];
+      // Crear un objeto para almacenar los datos
+      var tarjetaUsuario = {
+        numero : numero,
+        vencimiento : vencimiento,
+        cvc : cvc,
+        nombre : nombre
+      };
+       // Agregar el objeto de datos al arreglo de usuarios
+       usuario.push(tarjetaUsuario);
+    // Convertir el objeto a una cadena JSON y guardarlo en el localStorage
+    localStorage.setItem("tarjetaUsuario", JSON.stringify(tarjetaUsuario));
+    // Guardar el arreglo usuarios en el localStorage
+    localStorage.setItem("usuarios", JSON.stringify(usuario));
+        alert("Datos guardados correctamente en el localStorage.");
+    } else {
+         alert("Por favor, complete todos los campos del formulario.");
+    }
+}
+
+//guardarEnLocalStorage al evento click del botón
+btnPagar.addEventListener("click", guardarEnLocalStorage);
+
+
+
