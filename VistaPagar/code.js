ecfaeb4c62bba0c8de6a1a7034c55b55ca7913f0
milagrospessino <milipessi@gmail.com>
2023-11-06 16:19:07 -0300

    premium

--- VistaPagar/code.js
+++ VistaPagar/code.js
@@ -44,4 +44,37 @@ function guardarEnLocalStorage() {
 btnPagar.addEventListener("click", guardarEnLocalStorage);
 
 
+/*const titulo=document.querySelector("#titulo")
+const URLAbuscar=new URL(window.location.href)
+const nombrePlan=URLAbuscar.searchParams.get("plan")
+titulo.textContent+=nombrePlan*/
 
+
+function cambiarNombrePlan(){
+  const titulo = document.getElementById('titulo');
+  const precio = document.getElementById('precio');
+  const img = document.getElementById('imagenPlan');
+  const URLAbuscar=window.location.href;
+  const array = URLAbuscar.split('?')
+  let plan = array[array.length -1];
+
+  if(plan == "plan=1"){
+      titulo.innerHTML = "Elegiste el plan Mensual";
+      precio.innerHTML = "100$";
+      
+
+  }else if(plan == "plan=2"){
+      titulo.innerHTML = "Elegiste el plan Anual";
+      precio.innerHTML = "1000$";
+      img.innerHTML = '<img src="..\\vistaPremium\\media\\aniversario.png">';
+      
+  }else if(plan == "plan=3"){
+      titulo.innerHTML = "Elegiste el plan Infinito";
+      precio.innerHTML = "2000$";
+      img.innerHTML = '';
+      
+  }
+
+  
+  
+}
