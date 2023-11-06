764f88bfe908e335fa0b8ea456dfb337b004c99d
Ignacio Lopez Cabanillas <ignaciolopezcabanillas@gmail.com>
2023-11-06 20:12:50 -0300

    perfil

--- VistaPerfil/perfil.js
+++ VistaPerfil/perfil.js
@@ -1,24 +1,44 @@
-var usuarioData = {
-    nombre: "nombre_de_usuario",
-    contraseña: "contrasena",
-  };
-  
-  //localStorage.setItem("usuarioData", JSON.stringify(usuarioData));
+document.addEventListener("DOMContentLoaded", function() {
+  const formPerfil = document.getElementById("formPerfil");
+  const nombreUsuarioInput = document.getElementById("nombreUsuario");
+  const contraseñaInput = document.getElementById("contraseña");
+  const repetirContraseña = document.getElementById("repetirContraseña");
+  const emailInput = document.getElementById("email");
+  const fechaNacimientoInput = document.getElementById("fechaNacimiento");
 
-var usuarioDataGuardada = localStorage.getItem("usuarioData");
-var usuarioData = JSON.parse(usuarioDataGuardada);
+  // Cargar la información del usuario desde el localStorage
+  const usuariosLista = JSON.parse(localStorage.getItem("usuariosLista")) || [];
+  const usuarioActual = usuariosLista[0];
 
-document.getElementById("nombreUsuario").value = usuarioData.nombre;
-document.getElementById("contraseñaUsuario").value = usuarioData.contraseña;
+  //var usuario = JSON.parse(localStorage.getItem("usuario"));
+  //document.getElementById("usuarioLink").textContent = usuario.nombreUsuario;
 
-document.getElementsByClassName("botonGuardar").addEventListener("click", function() {
+  nombreUsuarioInput.value = usuarioActual.nombreUsuario;
+  contraseñaInput.value = usuarioActual.contraseña;
+  repetirContraseña.value = usuarioActual.contraseña;
+  emailInput.value = usuarioActual.email;
+  fechaNacimientoInput.value = usuarioActual.fechaNacimiento;
 
-  var nombreUsuario = document.getElementById("nombreUsuario").value;
-  var contraseñaUsuario = document.getElementById("contraseñaUsuario").value;
+  formPerfil.addEventListener("submit", function(event) {
+      event.preventDefault();
+      
+      // Actualizar la información del usuario con los valores de los campos de entrada
+      usuarioActual.nombreUsuario = nombreUsuarioInput.value;
+      usuarioActual.contraseña = invertirMitades(contraseñaInput.value);
+      usuarioActual.email = emailInput.value;
+      usuarioActual.fechaNacimiento = fechaNacimientoInput.value;
 
-  userData.username = newUsername;
-  userData.password = newPassword;
+      // Guardar la información actualizada en el localStorage
+      localStorage.setItem("usuariosLista", JSON.stringify(usuariosLista));
 
+      alert("Cambios guardados correctamente.");
+  });
 
-  localStorage.setItem("usuarioData", JSON.stringify(usuarioData));
+  function invertirMitades(contraseña) {
+      var mitad = Math.floor(contraseña.length / 2);
+      return contraseña.slice(mitad) + contraseña.slice(0, mitad);
+  }
+
+  // Nombre de usuario en el nav
+  document.getElementById("nombreLink").textContent = usuarioActual.nombreUsuario;
 });
