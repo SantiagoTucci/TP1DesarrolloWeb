f5eb62914a853d659fc26122891ba988038cf84b
milagrospessino <milipessi@gmail.com>
2023-11-06 17:51:41 -0300

    g

--- VistaPagar/code.js
+++ VistaPagar/code.js
@@ -34,17 +34,17 @@ const precioElemento = document.getElementById('precioPlan');
 const descripcionElemento = document.getElementById('textoPlan');
 
 // Lógica para mostrar la información según el plan seleccionado
-if (planSeleccionado === 'planMensual') {
+if (planSeleccionado == 'planMensual') {
   tituloElemento.textContent = 'Elegiste el plan Mensual';
   imagenElemento.src = 'img/30-dias.png';
   precioElemento.textContent = '$100';
   descripcionElemento.textContent = 'Nuestro plan Premium de un mes ofrece a los usuarios una experiencia premium sin compromisos a largo plazo. Con acceso ilimitado durante 30 días, los suscriptores pueden disfrutar de todas las características exclusivas de nuestro servicio.';
-} else if (planSeleccionado === 'planAnual') {
+} else if (planSeleccionado == 'planAnual') {
   tituloElemento.textContent = 'Elegiste el plan Anual';
   imagenElemento.src = 'img/aniversario.png';
   precioElemento.textContent = '$1000';
   descripcionElemento.textContent = 'Nuestro plan Premium anual es la opción ideal para aquellos que buscan una experiencia premium a largo plazo. Al suscribirte por un año completo, disfrutarás de acceso ilimitado y sin interrupciones a todas nuestras características exclusivas.';
-} else if(planSeleccionado === 'planInfinito'){
+} else if(planSeleccionado == 'planInfinito'){
   tituloElemento.textContent = 'Elegiste el plan Infinito';
   imagenElemento.src = 'img/infinidad.png';
   precioElemento.textContent = '$2000';
