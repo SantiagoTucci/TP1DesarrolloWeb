6075c4a69161b84bbe82fdb21e87aee7499533ee
milagrospessino <milipessi@gmail.com>
2023-11-06 21:08:26 -0300

    vistapagar

--- VistaPagar/code.js
+++ VistaPagar/code.js
@@ -31,21 +31,25 @@ const precioElemento = document.getElementById('precioPlan');
 const descripcionElemento = document.getElementById('textoPlan');
 
 function cambiarNombrePlan() {
+ /*
   const urlParams = new URLSearchParams(window.location.search);
-  const planSeleccionado = urlParams.get('plan');
+  const planSeleccionado = urlParams.get('plan');*/
+  const URLAbuscar=window.location.href;
+  const array = URLAbuscar.split('?')
+  let plan = array[array.length -1];
 
 // Lógica para mostrar la información según el plan seleccionado
-if (planSeleccionado === "1") {
-  tituloElemento.textContent = 'Elegiste el plan Mensual';
+if (plan == "plan=1") {
+  tituloElemento.innerHTML = 'Elegiste el plan Mensual';
   imagenElemento.src = 'img/30-dias.png';
-  precioElemento.textContent = '$100';
-  descripcionElemento.textContent = 'Nuestro plan Premium de un mes ofrece a los usuarios una experiencia premium sin compromisos a largo plazo. Con acceso ilimitado durante 30 días, los suscriptores pueden disfrutar de todas las características exclusivas de nuestro servicio.';
-} else if (planSeleccionado === '2') {
-  tituloElemento.textContent= 'Elegiste el plan Anual';
+  precioElemento.innerHTML = '$100';
+  descripcionElemento.innerHTML = 'Nuestro plan Premium de un mes ofrece a los usuarios una experiencia premium sin compromisos a largo plazo. Con acceso ilimitado durante 30 días, los suscriptores pueden disfrutar de todas las características exclusivas de nuestro servicio.';
+} else if (plan == 'plan=2') {
+  tituloElemento.innerHTML= 'Elegiste el plan Anual';
   imagenElemento.src = 'img/aniversario.png';
-  precioElemento.textContent = '$1000';
-  descripcionElemento.textContent = 'Nuestro plan Premium anual es la opción ideal para aquellos que buscan una experiencia premium a largo plazo. Al suscribirte por un año completo, disfrutarás de acceso ilimitado y sin interrupciones a todas nuestras características exclusivas.';
-} else if(planSeleccionado === '3'){
+  precioElemento.innerHTML = '$1000';
+  descripcionElemento.innerHTML = 'Nuestro plan Premium anual es la opción ideal para aquellos que buscan una experiencia premium a largo plazo. Al suscribirte por un año completo, disfrutarás de acceso ilimitado y sin interrupciones a todas nuestras características exclusivas.';
+} else if(plan == 'plan=3'){
   tituloElemento.textContent = 'Elegiste el plan Infinito';
   imagenElemento.src = 'img/infinidad.png';
   precioElemento.textContent = '$2000';
@@ -57,7 +61,7 @@ if (planSeleccionado === "1") {
 }
 };
 
-
+cambiarNombrePlan();
 
 //-----------------------------------------------------------------------------------
 
