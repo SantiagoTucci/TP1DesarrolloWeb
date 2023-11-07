2114d3c05d6dab8c64422d753efc822fa53f3e15
milagrospessino <milipessi@gmail.com>
2023-11-07 10:37:33 -0300

    inicio sesion

--- VistaMisFavoritos/favoritos.js
+++ VistaMisFavoritos/favoritos.js
@@ -1,10 +1,14 @@
+//NOMBRE DE USUARIO
 var usuario = JSON.parse(localStorage.getItem("usuario"));
-document.querySelector('.nav-link').textContent = usuario.nombreUsuario;
-     
-     function agregarAFavoritos(cancion){
+    document.querySelector('.nav-link').textContent = usuario.nombreUsuario;
 
-     }
-
+//CERRAR SESION
+document.getElementById('cerrarSesion').addEventListener('click', function() {
+    //autenticación como 'false' en el localStorage
+    localStorage.setItem('estadoCuenta', 'Se cerro sesion');
+    // Redirigir a la página de inicio de sesión
+    window.location.href = 'index.html';
+  }); 
     
 
 
