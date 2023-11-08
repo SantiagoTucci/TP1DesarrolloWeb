2f37aa70906963c49d5ff9119c35eb281e3f752d
milagrospessino <milipessi@gmail.com>
2023-11-08 03:13:26 -0300

    s

--- VistaMusicaSonando/musicasonando.js
+++ VistaMusicaSonando/musicasonando.js
@@ -11,8 +11,9 @@ localStorage.setItem('estadoCuenta', 'Se cerro sesion');
 window.location.href = 'index.html';
 }); 
 
-//MARCAR CANCIONES COMO FAVORITAS 
 
+
+//MARCAR CANCIONES COMO FAVORITAS 
 var cancionesFavoritas = JSON.parse(localStorage.getItem(user.nombreUsuario + "cancionesFav")) || [];
   
 document.querySelectorAll('.star-icon').forEach(function(estrella) {
@@ -42,50 +43,6 @@ document.querySelectorAll('.star-icon').forEach(function(estrella) {
       });
   }); 
 
-var cancionesuser = JSON.parse(localStorage.getItem(user.nombreUsuario + "cancionesFav"));
-console.log(cancionesuser)
-  const estrella = document.querySelectorAll(".star-icon")
+
   
 
-// al hacer click en una estrella agrega la cancion a el array de cancionesFav en el usuario
-
-/*Array.from(estrella).forEach(function (estrella) {
-    estrella.addEventListener("click", function (event) {
-        event.preventDefault(); // Prevenir la acción predeterminada del evento
-        event.stopPropagation(); // Detiene la propagación del evento
-            estrella.style.filter = 'invert(71%) sepia(99%) saturate(1350%) hue-rotate(358deg) brightness(103%) contrast(105%)';
-
-            for (const user of usuariosRegistrados) {
-                const Cancion = this.id
-                let cancionesFav = user.cancionesFav
-                const contieneCancion = cancionesFav.some((cancionbuscada) => cancionbuscada === Cancion)
-                if (contieneCancion) {
-                    const indiceCancion = cancionesFav.indexOf(Cancion)
-                    cancionesFav.splice(indiceCancion, 1)
-                    estrella.style.filter = 'invert(100%) sepia(0%) saturate(7500%) hue-rotate(100deg) brightness(115%) contrast(106%)';
-                } else {
-                    cancionesFav.push(Cancion);
-                }
-            
-        }
-
-        localStorage.setItem("usuarios", JSON.stringify(usuariosRegistrados));
-
-
-    })
-
-})
-
-for (const user of usuariosRegistrados) {
-    const cancionesFav = user.cancionesFav
-    estrella.forEach(element => {
-        
-        if (cancionesFav.includes(element.id)) {
-          console.log(element.id);
-          estrella.style.filter = 'invert(71%) sepia(99%) saturate(1350%) hue-rotate(358deg) brightness(103%) contrast(105%)'; 
-          
-      }
-
-    });
-
-}*/
