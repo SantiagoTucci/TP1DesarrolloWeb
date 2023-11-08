55c02a4466b63a80868a93f0eef6d9e9d661636d
Santiago <142843041+SantiagoTucci@users.noreply.github.com>
2023-11-08 01:20:11 -0300

    vistaMisAlbums

--- VistaMisAlbums/misAlbumes.js
+++ VistaMisAlbums/misAlbumes.js
@@ -1,9 +1,3 @@
-//NOMBRE DE USUARIO
-window.onload = function(){
-    var usuario = JSON.parse(localStorage.getItem("usuario"));
-      document.querySelector('.nav-link').textContent = usuario.nombreUsuario;
-  }
-
 //CERRAR SESION
 document.getElementById('cerrarSesionAlbum').addEventListener('click', function() {
     //autenticación como 'false' en el localStorage
@@ -12,4 +6,48 @@ document.getElementById('cerrarSesionAlbum').addEventListener('click', function(
     window.location.href = 'index.html';
   });
 
-  
+  //NOMBRE DE USUARIO
+window.onload = function(){
+  var usuario = JSON.parse(localStorage.getItem("usuario"));
+    document.querySelector('.nav-link').textContent = usuario.nombreUsuario;
+
+
+  //CANCION ACTUAL
+  document.querySelector('.audio-player').addEventListener('play', function() {
+    var cancionActual = this.querySelector('source').getAttribute('src');
+    localStorage.setItem("cancionActual", cancionActual);
+});
+
+  //RETIRAR Y SELECCIONAR ALBUM FAVORITO
+    var usuario = JSON.parse(localStorage.getItem("usuario"));
+    document.querySelector('.nav-link').textContent = usuario.nombreUsuario;
+
+    var albumesFavoritos = JSON.parse(localStorage.getItem(usuario.nombreUsuario + "albumesFavoritos")) || [];
+
+    document.querySelectorAll('.star-icon').forEach(function(estrella) {
+        var albumId = estrella.parentElement.getAttribute('data-id');
+
+        if (albumesFavoritos.includes(albumId)) {
+            estrella.style.filter = 'invert(71%) sepia(99%) saturate(1350%) hue-rotate(358deg) brightness(103%) contrast(105%)'; // Amarillo
+        }
+
+        estrella.addEventListener('click', function(event) {
+            event.preventDefault(); // Prevenir la acción predeterminada del evento
+            event.stopPropagation(); // Detiene la propagación del evento
+
+            if (albumesFavoritos.includes(albumId)) {
+                // Retirar el álbum de los favoritos
+                albumesFavoritos = albumesFavoritos.filter(function(favorito) {
+                    return favorito !== albumId;
+                });
+                estrella.style.filter = '';
+            } else {
+                // Agregar el álbum a los favoritos
+                albumesFavoritos.push(albumId);
+                estrella.style.filter = 'invert(71%) sepia(99%) saturate(1350%) hue-rotate(358deg) brightness(103%) contrast(105%)'; // Amarillo
+            }
+
+            localStorage.setItem(usuario.nombreUsuario + "albumesFavoritos", JSON.stringify(albumesFavoritos));
+        });
+    });
+  };
