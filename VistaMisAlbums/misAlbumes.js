ae1f4770c3b7f826a88f437d050c9761edac0c53
Santiago <142843041+SantiagoTucci@users.noreply.github.com>
2023-11-08 14:40:57 -0300

    vistaMisAlbums

--- VistaMisAlbums/misAlbumes.js
+++ VistaMisAlbums/misAlbumes.js
@@ -1,5 +1,76 @@
 //ETIQUETAS
- var albumes = document.querySelectorAll('.main-card-album');
+const albums = [
+  {
+    id: "album-01",
+    imageUrl: "..\Musica\Fotos\Álbumes\Abbey Road.jpg",
+    title: "Abbey Road",
+    artist: "The Beatles",
+    href: "..\VistaMusicaSonando\thebeatles.html",
+  },
+  {
+    id: "album-02",
+    imageUrl: "..\Musica\Fotos\Álbumes\Canción Animal.jpg",
+    title: "Canción Animal",
+    artist: "Soda Stereo",
+    href: "..\VistaMusicaSonando\thebeatles.html",
+  },
+  {
+    id: "album-03",
+    imageUrl: "..\Musica\Fotos\Álbumes\Artaud.jpg",
+    title: "Artaud",
+    artist: "Pescado Rabioso",
+    href: "..\VistaMusicaSonando\thebeatles.html",
+  },
+  {
+    id: "album-04",
+    imageUrl: "..\Musica\Fotos\Álbumes\Locura.jpg",
+    title: "Locura",
+    artist: "Virus",
+    href: "..\VistaMusicaSonando\thebeatles.html",
+  },
+  {
+    id: "album-05",
+    imageUrl: "..\Musica\Fotos\Álbumes\Fuerza Natural.jpg",
+    title: "Fuerza Natural",
+    artist: "Gustavo Cerati",
+    href: "..\VistaMusicaSonando\thebeatles.html",
+  },
+  {
+    id: "album-06",
+    imageUrl: "..\Musica\Fotos\Álbumes\Nevermind.jpg",
+    title: "Nevermind",
+    artist: "Nirvana",
+    href: "..\VistaMusicaSonando\thebeatles.html",
+  },
+  {
+    id: "album-07",
+    imageUrl: "..\Musica\Fotos\Álbumes\Raro.jpg",
+    title: "Raro",
+    artist: "El Cuarteto de Nos",
+    href: "..\VistaMusicaSonando\thebeatles.html",
+  },
+  {
+    id: "album-08",
+    imageUrl: "..\Musica\Fotos\Álbumes\Never Gonna Give You Up.jpg",
+    title: "Whenever You Need Somebody",
+    artist: "Rick Astley",
+    href: "..\Musica\Video\Rick Roll (Different link + no ads) [xvFZjo5PgG0].mp4",
+  },
+  {
+    id: "album-09",
+    imageUrl: "..\Musica\Fotos\Álbumes\Peperina.webp",
+    title: "Peperina",
+    artist: "Serú Girán",
+    href: "..\VistaMusicaSonando\thebeatles.html",
+  },
+  {
+    id: "album-10",
+    imageUrl: "..\Musica\Fotos\Álbumes\Giros.jpg",
+    title: "Giros",
+    artist: "Fito Páez",
+    href: "..\VistaMusicaSonando\thebeatles.html",
+  },
+];
 
 //CERRAR SESION
 document.getElementById('cerrarSesionAlbum').addEventListener('click', function() {
@@ -14,110 +85,65 @@ window.onload = function(){
   var usuario = JSON.parse(localStorage.getItem("usuario"));
     document.querySelector('.nav-link').textContent = usuario.nombreUsuario;
 
-
   //CANCION ACTUAL
   document.querySelector('.audio-player').addEventListener('play', function() {
     var cancionActual = this.querySelector('source').getAttribute('src');
     localStorage.setItem("cancionActual", cancionActual);
 });
 
-  //RETIRAR Y SELECCIONAR ALBUM FAVORITO
-    var usuario = JSON.parse(localStorage.getItem("usuario"));
-    document.querySelector('.nav-link').textContent = usuario.nombreUsuario;
+var albumesFavoritos = JSON.parse(localStorage.getItem(usuario.nombreUsuario + "albumesFavoritos")) || [];
+var arrayAlbums = albums;
+function mostrarAlbumesFavoritos(arrayAlbums, albumesFavoritos) {
+  // Obtiene los elementos DOM de los álbumes
+  const albumContainers = document.querySelectorAll(".album-container");
 
-    var albumesFavoritos = JSON.parse(localStorage.getItem(usuario.nombreUsuario + "albumesFavoritos")) || [];
+  // Recorre los elementos DOM de los álbumes
+  for (const albumContainer of albumContainers) {
+    // Obtiene el ID del álbum
+    const albumId = albumContainer.getAttribute("data-id");
 
-    document.querySelectorAll('.star-icon').forEach(function(estrella) {
-        var albumId = estrella.parentElement.getAttribute('data-id');
-
-        if (albumesFavoritos.includes(albumId)) {
-            estrella.style.filter = 'invert(71%) sepia(99%) saturate(1350%) hue-rotate(358deg) brightness(103%) contrast(105%)'; // Amarillo
-        }
-
-        estrella.addEventListener('click', function(event) {
-            event.preventDefault(); // Prevenir la acción predeterminada del evento
-            event.stopPropagation(); // Detiene la propagación del evento
-
-            if (albumesFavoritos.includes(albumId)) {
-                // Retirar el álbum de los favoritos
-                albumesFavoritos = albumesFavoritos.filter(function(favorito) {
-                    return favorito !== albumId;
-                });
-                estrella.style.filter = '';
-            } else {
-                // Agregar el álbum a los favoritos
-                albumesFavoritos.push(albumId);
-                estrella.style.filter = 'invert(71%) sepia(99%) saturate(1350%) hue-rotate(358deg) brightness(103%) contrast(105%)'; // Amarillo
-                actualizarVistaMisAlbumes(albumsSeleccionados);
-            }
-
-            localStorage.setItem(usuario.nombreUsuario + "albumesFavoritos", JSON.stringify(albumesFavoritos));
-       
-        });
-    });
-
-
-    function actualizarVistaMisAlbumes(albumesFavoritos) {
-      // Obtén el contenedor donde mostrarás los álbumes seleccionados en la vista Mis Álbumes
-      var vistaMisAlbumesContainer = document.getElementById('main-flex-albums');
-      // Limpia el contenido existente en el contenedor
-      vistaMisAlbumesContainer.innerHTML = '';
-      // Verifica si hay álbumes seleccionados
-      if (albumesFavoritos.length > 0) {
-          // Itera sobre la lista de álbumes seleccionados y crea elementos para cada álbum
-          albumesFavoritos.forEach(function(albumId) {
-              // Recupera información del álbum a partir de su ID (puedes tener una estructura de datos)
-              var album = obtenerInformacionDelAlbumPorId(albumId); // Implementa esta función según tu estructura de datos
-              // Crea elementos de álbum para mostrar en la vista Mis Álbumes
-              var albumElemento = document.createElement('div');
-              albumElemento.className = 'album-seleccionado';
-              albumElemento.innerHTML = `
-                  <img src="${album.imagen}" alt="${album.nombre}" class="album-imagen">
-                  <p class="album-nombre">${album.nombre}</p>
-                  <p class="album-artista">${album.artista}</p>
-              `;
-              // Agrega el elemento del álbum al contenedor de Mis Álbumes
-              vistaMisAlbumesContainer.appendChild(albumElemento);
-          });
-      } else {
-          // Si no hay álbumes seleccionados, muestra un mensaje indicando que no hay álbumes
-          vistaMisAlbumesContainer.innerHTML = '<p>No hay álbumes seleccionados.</p>';
-      }
+    // Si el álbum está en la lista de favoritos, lo muestra
+    if (albumesFavoritos.includes(albumId)) {
+      albumContainer.style.display = "block";
+    } else {
+      albumContainer.style.display = "none";
+    }
   }
-  
-//------------------------LIMPIAR LOS ALBUMES EN HTML---------------------------------------------------------
-  function obtenerIdsAlbumesEnMisAlbumes() {
-    // Obtiene los IDs de los álbumes desde el localStorage
-   var idsAlbumesEnMisAlbumes = JSON.parse(localStorage.getItem('misAlbumes')) || [];
-   return idsAlbumesEnMisAlbumes;
-  }
-
-  var idsAlbumesEnMisAlbumes = obtenerIdsAlbumesEnMisAlbumes();
-  var contenedorAlbumes = document.querySelector('.main-flex-albums');
-
-  // Obtén todos los elementos de álbumes en main-flex-albums
-  var albumes = contenedorAlbumes.querySelectorAll('.main-card-album');
-
-    albumes.forEach(function(album) {
-     var albumId = album.querySelector('.album-container').getAttribute('data-id');
-
-      // Verifica si el álbum no está en la lista de IDs de álbumes en misAlbumes
-      if (!idsAlbumesEnMisAlbumes.includes(albumId)) {
-         // Si no está en misAlbumes, elimina el álbum del DOM
-         album.remove();
-     } else {
-        var albumElemento = document.createElement('<div>');
-        albumElemento.className = 'album-seleccionado';
-        albumElemento.innerHTML = 
-        ` <img src="${album.imagen}" alt="${album.nombre}" class="album-imagen">
-          <p class="album-nombre">${album.nombre}</p>
-          <p class="album-artista">${album.artista}</p>
-        `;
-      // Agrega el elemento del álbum al contenedor de Mis Álbumes
-      contenedorAlbumes.appendChild(albumElemento);
-     }
-  });
+}
 
 };
+
+
+//------------------------LOS ALBUMES EN HTML---------------------------------------------------------
+
+
+
+
+/*function insertarAlbum(albums, albumId) {
+  const album = albums.find((album) => album.id === albumId);
+
+  if (album === undefined) {
+    return null;
+  }
+
+  // Inserta la información del álbum en el DOM
+  const albumContainer = document.querySelector(`.album-container[data-id="${albumId}"]`);
+  albumContainer.querySelector(`img.main-card-album-image`).src = album.imageUrl;
+  albumContainer.querySelector(`p.main-card-album-text`).textContent = album.title;
+  albumContainer.querySelector(`p.main-card-album-subtext`).textContent = album.artist;
+
+  return album;
+}
+*/
+/*document.querySelectorAll('.star-icon').forEach(function(estrella) {
+  var albumId = estrella.parentElement.getAttribute('data-id');
+
+  if (albumesFavoritos.includes(albumId)) {
+      insertarAlbum(albums, albumId);
+  } else {
+    albumesFavoritos.remove();
+  }
+});*/
+
 
   
